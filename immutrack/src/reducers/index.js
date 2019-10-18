1cb39a2630852872bb4454d63b68be306bc6776d
Jason Loomis <jason@jrloomis.com>
2019-10-18 11:10:12 -0400

    refactored to use combineReducers

--- immutrack/src/reducers/index.js
+++ immutrack/src/reducers/index.js
@@ -1,26 +1,6 @@
-const initialState = {
-    childList: [
-        /* 
-            {
-                name: [type text]
-                age: [type text]
-                social: [type text]
-                signUpCode: [type text] //do I need to keep this in state?
-                immunizations: [
-                    {
-                        immunizationName: [type text]
-                        DateOfVaccination: [type text]
-                        hasShot: [type bool]
-                    }
-                ]
-            }
-        */
-    ]
-}
+import { combineReducers } from "redux";
+import { loginReducer } from "./loginReducers";
 
-export const reducer = (state = initialState, action) => {
-    switch(action.type){
-        default:
-            return state;
-    }
-}
+export default combineReducers({
+  loginReducer
+});
