c7aa72741f131dd741666160568f7b5ee62d9e06
Jason Loomis <jason@jrloomis.com>
2019-10-18 11:04:52 -0400

    fixed path

--- immutrack/src/components/base/IntroPage.js
+++ immutrack/src/components/base/IntroPage.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const IntroPage = () => {
   return (
     <div>
-      <Link to="/user">Login</Link>
+      <Link to="/login">Login</Link>
     </div>
   );
 };
