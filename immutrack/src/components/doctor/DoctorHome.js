e20452523a48afec28358c520627b7bb3546357c
Aaron Williams <aaronrw4@yahoo.com>
2019-10-18 19:30:17 -0700

    Wrote JSX for Doctor Home page

--- immutrack/src/components/doctor/DoctorHome.js
+++ immutrack/src/components/doctor/DoctorHome.js
@@ -1,18 +1,19 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
-import {Route} from "react-router-dom";
-import {axiosWithAuth} from '../../utils';
+import {axiosWithAuth} from './axiosWithAuth';
+import {Link} from 'react-router-dom';
 
 export default function DoctorHome() {
-    const [patients, setPatients] = useState({});
-
+    const [patients, setPatients] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [searchResults, setSearchResults] = useState([]);
 
 useEffect(() => {
     const getPatients = () => {
         axiosWithAuth()
-            .get()
+            .get('https://immunization-tracker-bw.herokuapp.com/provider/1/children')
             .then(response => {
                 setPatients(response.data);
+                setSearchResults(response.data.results);
                 console.log(response.data);
             })
             .catch(err => {
@@ -20,7 +21,40 @@ useEffect(() => {
             });
     }
     getPatients();
-})
+}, [])
 
+useEffect (() => {
+    const results = patients.filter(patient =>
+        patient.name.includes(searchTerm)
+    );
+    setSearchResults(results);
+}, [searchTerm]);
 
+const handleChange = event => {
+    setSearchTerm(event.target.value);
+    console.log(searchTerm);
+  };
+
+return (
+    <div>
+        <form className='form'>
+        <input
+          id="name"
+          type="text"
+          name="textfield"
+          placeholder="Search"
+          value={searchTerm}
+          onChange={handleChange}
+          className='input'
+        />
+      </form>
+    <div>
+        {searchResults.map(patient => (
+            <Link to='#'>
+                <p>{patient.name}</p>
+            </Link>
+        ))}
+    </div>
+    </div>
+)
 }
