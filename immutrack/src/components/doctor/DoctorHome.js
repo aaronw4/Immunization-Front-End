9d74bc0542cd6c555bef5c8848dacf47f2e984e9
Aaron Williams <aaronrw4@yahoo.com>
2019-10-18 18:20:19 -0700

    Wrote useEffect

--- immutrack/src/components/doctor/DoctorHome.js
+++ immutrack/src/components/doctor/DoctorHome.js
@@ -3,7 +3,24 @@ import axios from "axios";
 import {Route} from "react-router-dom";
 import {axiosWithAuth} from '../../utils';
 
+export default function DoctorHome() {
+    const [patients, setPatients] = useState({});
+
+
 useEffect(() => {
-    axiosWithAuth()
-    .get
+    const getPatients = () => {
+        axiosWithAuth()
+            .get()
+            .then(response => {
+                setPatients(response.data);
+                console.log(response.data);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+    getPatients();
 })
+
+
+}
