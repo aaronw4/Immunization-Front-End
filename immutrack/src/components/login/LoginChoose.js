b7cbcc2121abda4cf170fa9266714dacaa6ba0a9
Jason Loomis <jason@jrloomis.com>
2019-10-19 04:38:02 -0400

    passing user type instead of hard coded in actions

--- immutrack/src/components/login/LoginChoose.js
+++ immutrack/src/components/login/LoginChoose.js
@@ -5,13 +5,13 @@ import { loginParent, loginProvider } from "../../actions/loginType";
 const LoginChoose = ({ history, loginParent, loginProvider }) => {
   const parent = e => {
     e.preventDefault();
-    loginParent();
+    loginParent("parents");
     history.push("/parent-login");
   };
 
   const provider = e => {
     e.preventDefault();
-    loginProvider();
+    loginProvider("providers");
     history.push("/provider-login");
   };
 
