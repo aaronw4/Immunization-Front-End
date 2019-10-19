a5541c8b5cf0ce6c0d35ae5397bd15f6b9fb5bdb
Aaron Williams <aaronrw4@yahoo.com>
2019-10-18 21:23:01 -0700

    Built DHP patient buttons and styles

--- immutrack/src/components/doctor/DoctorHome.js
+++ immutrack/src/components/doctor/DoctorHome.js
@@ -1,6 +1,35 @@
 import React, { useEffect, useState } from "react";
 import {axiosWithAuth} from './axios';
 import {Link, Route} from 'react-router-dom';
+import styled from 'styled-components';
+
+const PatientCont = styled.div`
+    margin-top: 40px;
+`;
+
+const PatientButton = styled.div`
+    width: 350px;
+    height: 50px;
+    left: 32px;
+    top: 191px;    
+    background: #C4C4C4;
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 10px;
+    margin-left: auto;
+    margin-right: auto;
+    align-items: center;
+`;
+
+const Record = styled.button`
+    background: #848484;
+    border-radius: 20px;
+    width: 80px;
+    height: 25px;
+    justify-content: center;
+    margin-right: 10px;
+`;
+
 
 export default function DoctorHome() {
     const [patients, setPatients] = useState([]);
@@ -23,8 +52,9 @@ useEffect(() => {
     getPatients();
 }, [])
 
-useEffect (() => {
+useEffect (() => {    
     const results = patients.filter(patient =>
+        
         patient.firstName.includes(searchTerm) || patient.lastName.includes(searchTerm)
     );
     setSearchResults(results);
@@ -32,29 +62,32 @@ useEffect (() => {
 
 const handleChange = event => {
     setSearchTerm(event.target.value);
-    console.log(searchTerm);
   };
 
 return (
     <div>
         <Route exact path='/'>
+        <h2>Patient Status</h2>
         <form className='form'>
             <input
             id="name"
             type="text"
             name="textfield"
-            placeholder="Search"
+            placeholder="&#x1F50D;"
             value={searchTerm}
             onChange={handleChange}
             className='input'
             />
         </form>     
-        
-        {searchResults.map(patient => (
-            <Link to='#'>
+        <PatientCont>
+            {searchResults.map(patient => (            
+                <PatientButton>
+                <button>xxxx</button>
                 <p>{patient.firstName} {patient.lastName}</p>
-            </Link>
-        ))}
+                <Record>Record</Record>
+                </PatientButton>            
+            ))}
+        </PatientCont>
          </Route>
     </div>
 )
