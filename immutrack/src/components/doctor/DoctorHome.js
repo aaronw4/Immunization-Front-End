67ee3ad6119e0bdc169057014da1c3e06e9e686d
Aaron Williams <aaronrw4@yahoo.com>
2019-10-18 17:01:22 -0700

    Set up page

---
+++ immutrack/src/components/doctor/DoctorHome.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import {Route} from "react-router-dom";
+import {axiosWithAuth} from '../../utils';
+
+useEffect(() => {
+    axiosWithAuth()
+    .get
+})
