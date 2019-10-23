a804ad17ee8daf5be85bf463adecec5aa6cc55af
Jason Loomis <jason@jrloomis.com>
2019-10-23 00:27:59 -0400

    removed footer path - will be rendered by App.js

--- immutrack/src/utils/Router.js
+++ immutrack/src/utils/Router.js
@@ -3,27 +3,26 @@ import { Route, Switch } from "react-router-dom";
 import IntroPage from "../components/base/IntroPage";
 import LoginChoose from "../components/login/LoginChoose";
 import LoginForm from "../components/login/Login";
-import Footer from '../components/base/Footer';
-import DoctorHome from '../components/doctor/DoctorHome';
-import PateintHome from '../components/patient/PatientHome';
-import AddChild from '../components/patient/AddChild';
+import Footer from "../components/base/Footer";
+import DoctorHome from "../components/doctor/DoctorHome";
+import PateintHome from "../components/patient/PatientHome";
+import AddChild from "../components/patient/AddChild";
 import PatientRegisterForm from "../components/patient/PatientRegister";
-import Permissions from '../components/patient/Permissions';
+import Permissions from "../components/patient/Permissions";
 
 const Router = () => {
   return (
     <>
       <Switch>
         <Route exact path="/" component={IntroPage} />
-        <Route path='/' component={Footer}/>
         <Route path="/login" component={LoginChoose} />
         <Route path="/parent-login" component={LoginForm} />
         <Route path="/provider-login" component={LoginForm} />
-        <Route path='/doctor-home' component={DoctorHome} />
-        <Route path='/patient-home' component={PateintHome} />
-        <Route path='/add-child' component={AddChild} />
+        <Route path="/doctor-home" component={DoctorHome} />
+        <Route path="/patient-home" component={PateintHome} />
+        <Route path="/add-child" component={AddChild} />
         <Route path="/patient-register" component={PatientRegisterForm} />
-        <Route path='/permissions' component={Permissions} />
+        <Route path="/permissions" component={Permissions} />
       </Switch>
     </>
   );
