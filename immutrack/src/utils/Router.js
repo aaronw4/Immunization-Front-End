0045fec48548f918d1e27ffbfe151a42399c38d5
Jason Loomis <jason@jrloomis.com>
2019-10-23 01:05:26 -0400

    removed unused

--- immutrack/src/utils/Router.js
+++ immutrack/src/utils/Router.js
@@ -3,7 +3,6 @@ import { Route, Switch } from "react-router-dom";
 import IntroPage from "../components/base/IntroPage";
 import LoginChoose from "../components/login/LoginChoose";
 import LoginForm from "../components/login/Login";
-import Footer from "../components/base/Footer";
 import DoctorHome from "../components/doctor/DoctorHome";
 import PateintHome from "../components/patient/PatientHome";
 import AddChild from "../components/patient/AddChild";
@@ -15,13 +14,14 @@ const Router = () => {
     <>
       <Switch>
         <Route exact path="/" component={IntroPage} />
+        <Route path="/patient-register" component={PatientRegisterForm} />
         <Route path="/login" component={LoginChoose} />
         <Route path="/parent-login" component={LoginForm} />
         <Route path="/provider-login" component={LoginForm} />
+        {/* Private Routes below */}
         <Route path="/doctor-home" component={DoctorHome} />
         <Route path="/patient-home" component={PateintHome} />
         <Route path="/add-child" component={AddChild} />
-        <Route path="/patient-register" component={PatientRegisterForm} />
         <Route path="/permissions" component={Permissions} />
       </Switch>
     </>
